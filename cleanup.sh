3a3a37dbeda368d804f8da1eb1283318fa774e9e
Lizardite Népouite <lizardite277@gmail.com>
2015-08-03 16:38:29 +0200

    Improved cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,18 @@
 alias fnh='find . -not -path "*/\.*" -exec echo " - {}" \;'
 
 echo "Cleaning paths"
-fnh -type f -iname "*.svg" -exec sed -i -e "s/\"\(file:\/\/\)\?[A-Z]:\\\\.*\\\/\"/g" {} \;
+fnh -type f -iname "*.svg" -exec sed -i '
+
+# Removes line feeds between parameters in XML tags
+:a /<[^>]*$/N; s/\n\s*/ /g; ta;
+
+# Trims absolute paths, converting them to relative paths
+s/"file:\/\/[A-Z]:\\[^"]*\\/"/g;
+
+# Hides layers named "whatever[nsfw]"
+s/<g\b[^>]*\binkscape:label=".*\[nsfw\]"/&/g; t nsfw; b; :nsfw s/style="display:none"/&/; t; s/>/ style="display:none">/
+
+' {} \;
 
 echo "Fixing permissions"
 fnh -type f -not -name "*.sh" -exec chmod 664 {} \;
